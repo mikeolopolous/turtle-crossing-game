8d2a508e145f6af3b77207ba687f5897b8139ce3
Miguel Lopez <ing.miguell@gmail.com>
2023-04-19 14:24:12 -0600

    agrego clase Player

--- player.py
+++ player.py
@@ -1,7 +1,19 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    
+    def __init__(self, shape="turtle", color="dodger blue"):
+        super().__init__()
+        self.shape(shape)
+        self.color(color)
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        self.forward(MOVE_DISTANCE)
